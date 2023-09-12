f5d6ab33cb0963533cda821d2bad9cd5dbdbfc38
Evgeny Tsygankov <cygankov.evgeniy7@wb.ru>
2023-09-12 16:35:10 +0300

    refactor(components): fix page titles
    translate page titles

--- src/pages/todo-list/index.tsx
+++ src/pages/todo-list/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { RouteNode } from '@wildberries/service-router';
 import { ReduxStoreLoader } from '@mihanizm56/redux-core-modules';
+import i18next from 'i18next';
 import { AppLayout } from '@/_layouts/app-layout';
+import { todoLocalizationMap as i18nKeyMap } from '@/pages/todo-list/page/_localization/localization-map';
 import { ListPage } from './page';
 import { TODO_LIST_PAGE_NAME } from './page/_constants';
 import { storeInjectConfig } from './store-inject-config';
@@ -9,7 +11,7 @@ import { storeInjectConfig } from './store-inject-config';
 const pageNode = TODO_LIST_PAGE_NAME;
 
 const action = async ({ store, toState, fromState }) => ({
-  title: 'Todo',
+  title: i18next.t(i18nKeyMap.titles.list),
   content: (
     <AppLayout>
       <RouteNode nodeName={pageNode}>
--- src/pages/todo-list/page/children/create-list-item/index.tsx
+++ src/pages/todo-list/page/children/create-list-item/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { RouteNode } from '@wildberries/service-router';
 import { ReduxStoreLoader } from '@mihanizm56/redux-core-modules';
+import i18next from 'i18next';
 import { AppLayout } from '@/_layouts/app-layout';
+import { todoLocalizationMap as i18nKeyMap } from '@/pages/todo-list/page/_localization/localization-map';
 import { CreatePage } from './page';
 import { CREATE_ITEM_PAGE_PAGE_NODE } from './_constants';
 import { storeInjectConfig } from './store-inject-config';
@@ -9,7 +11,7 @@ import { storeInjectConfig } from './store-inject-config';
 const pageNode = CREATE_ITEM_PAGE_PAGE_NODE;
 
 const action = async ({ fromState, toState }) => ({
-  title: 'CreateNewItem',
+  title: i18next.t(i18nKeyMap.titles.create),
   content: (
     <AppLayout>
       <RouteNode nodeName={pageNode}>
